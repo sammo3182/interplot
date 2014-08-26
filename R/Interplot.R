2214fe3048560dcd889f3f2658da14f5b3cb94b7
Yue Hu <yue-hu-1@uiowa.edu>
2014-08-26 12:38:35 -0500

    8/26_Hu License changed to MIT

---
+++ R/Interplot.R
@@ -0,0 +1,92 @@
+#' INTERPLOT: Plot Interactive Effects
+#' 
+#' interplot is the basic plotting function in the interplot package. It is a convenient function for creating graphs describing how the coefficient of one variable in a two-way interaction term will change along with the changes of the other variable.
+#' @param m A R object storing the result of a regression including at least one interaction term.
+#' @param var1 The target variable in the interaction term of interest, whose coefficient changes is intended to plot.
+#' @param var2 The changing variable in the interaction term of interest, along with the changes of which the changes of the coefficient of the other variable is shown in the plot.
+#' @param xlab A title for the x axis.
+#' @param ylab A title for the y axis.
+#' @param seed An arbitrary numeric value.
+#' @param sims Number of independent simulation draws to create.
+#' @param steps Desired length of the sequence. A non-negative number, which for seq and seq.int will be rounded up if fractional.
+#' @param plot A logical value to deside the output is a plot or a list of the coefficient, upper and lower bound of var2.
+#' @export
+#' @examples
+#' # Create continuous, dummy, and group level variables.
+#' y<-rnorm(100, 1, 2)              
+#' x<-log(abs(y))                   
+#' z<-y^2
+#' d<-ifelse(y<1, 0, 1)
+#' m<-ifelse(y<c(-3), 4,
+#'           ifelse(y<0, 3,
+#'                  ifelse(y<3, 2, 1)))
+#'                  
+#' # Apply the interplot to different regressions
+#' library(Interplot)
+#' 
+#' ## 1. OLS
+#' m1<-lm(y~x+d+z+x:z)
+#' interplot(m1, "x", "z")
+#' 
+#' ## 2. Logit with two interaction terms (the second term is of interest)
+#' m2<-glm(d~y+x+z+x:z+y:z, family=binomial(link="logit"))
+#' interplot(m2, "y", "z")
+#' 
+#' ## 3. Multilevel
+#' m3<-lmer(y~x+d+z+x:z+(1|m))
+#' interplot(m3, "x","z")
+
+
+
+
+interplot <- function(m, var1, var2, xlab=NULL, ylab=NULL, 
+                      seed=313, sims=1000, steps=100, plot=TRUE) {
+  require(arm)
+  require(ggplot2)
+  set.seed(seed)
+  m.sims <- arm::sim(m, sims)
+  var12 <- paste0(var2,":",var1)
+  if(class(m)!="lmerMod"){
+    if (!var12 %in% names(m$coef)) var12 <- paste0(var1,":",var2)
+    if (!var12 %in% names(m$coef)) stop(paste("Model does not include the interaction of",var1 ,"and",var2))
+    coef <- data.frame(fake = seq(min(m$model[var2], na.rm=T), max(m$model[var2], na.rm=T), length.out=steps), coef1 = NA, ub = NA, lb = NA)
+    
+    for(i in 1:steps) {   
+      coef$coef1[i] <- mean(m.sims@coef[,match(var1, names(m$coef))] + 
+                              coef$fake[i]*m.sims@coef[,match(var12, names(m$coef))])
+      coef$ub[i] <- quantile(m.sims@coef[,match(var1, names(m$coef))] + 
+                               coef$fake[i]*m.sims@coef[,match(var12, names(m$coef))], .975)
+      coef$lb[i] <- quantile(m.sims@coef[,match(var1, names(m$coef))] + 
+                               coef$fake[i]*m.sims@coef[,match(var12, names(m$coef))], .025)    
+    }
+  }else{
+    if (!var12 %in% unlist(dimnames(m@pp$X)[2])) var12 <- paste0(var1,":",var2)
+    if (!var12 %in% unlist(dimnames(m@pp$X)[2])) stop(paste("Model does not include the interaction of",var1 ,"and",var2))
+    coef <- data.frame(fake = seq(min(m@frame[var2], na.rm=T), max(m@frame[var2], na.rm=T), length.out=steps), coef1 = NA, ub = NA, lb = NA)
+    
+    for(i in 1:steps) {   
+      coef$coef1[i] <- mean(m.sims@fixef[,match(var1, unlist(dimnames(m@pp$X)[2]))] + 
+                              coef$fake[i]*m.sims@fixef[,match(var12, unlist(dimnames(m@pp$X)[2]))])
+      coef$ub[i] <- quantile(m.sims@fixef[,match(var1, unlist(dimnames(m@pp$X)[2]))] + 
+                               coef$fake[i]*m.sims@fixef[,match(var12, unlist(dimnames(m@pp$X)[2]))], .975)
+      coef$lb[i] <- quantile(m.sims@fixef[,match(var1, unlist(dimnames(m@pp$X)[2]))] + 
+                               coef$fake[i]*m.sims@fixef[,match(var12, unlist(dimnames(m@pp$X)[2]))], .025)    
+    }   
+  }
+  if(plot==TRUE) {
+    if(steps>5) {
+      coef.plot <- ggplot(coef, aes(x = fake, y = coef1)) +                       
+        geom_line() + geom_ribbon(aes(ymin=lb, ymax=ub), alpha=.5) + theme_bw()+
+        ylab(ylab) + xlab(xlab)
+    } else {
+      coef.plot <- ggplot(coef, aes(x = fake, y = coef1)) +                       
+        geom_point() + geom_errorbar(aes(ymin=lb, ymax=ub), width=0) + 
+        scale_x_continuous(breaks = 0:steps) + theme_bw()+
+        ylab(ylab) + xlab(xlab)
+    }
+    return(coef.plot)
+  } else {
+    names(coef) <- c(var2, "coef", "ub", "lb")
+    return(coef)
+  }
+}
