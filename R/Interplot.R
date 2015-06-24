0851661f2685164491e90098873b6b084f6c97fb
Frederick Solt <fsolt@mac.com>
2015-06-24 15:08:05 -0500

    modify examples

--- R/Interplot.R
+++ R/Interplot.R
@@ -41,13 +41,18 @@
 #' 
 #' # Plot interactions with a continous conditioning variable
 #' interplot(m = m_cyl, var1 = "cyl", var2 = "wt") +
-#' ggtitle("The Conditional Effect of Weight \n 
-#' on the Influence of Cylinder Number") + 
-#' theme(plot.title = element_text(face="italic"))
+#' xlab("Automobile Weight (thousands lbs)") +
+#' ylab("Estimated Coefficient for Number of Cylinders") +
+#' ggtitle("Estimated Coefficient of Engine Cylinders\non Mileage by Automobile Weight") +
+#' theme(plot.title = element_text(face="bold"))
 #' 
 #' 
 #' # Plot interactions with a categorical conditioning variable
-#' interplot(m = m_cyl, var1 = "wt", var2 = "cyl")
+#' interplot(m = m_cyl, var1 = "wt", var2 = "cyl") +
+#' xlab("Number of Cylinders") +
+#' ylab("Estimated Coefficient for Automobile Weight (thousands lbs)") +
+#' ggtitle("Estimated Coefficient of Automobile Weight \non Mileage by Engine Cylinders") +
+#' theme(plot.title = element_text(face="bold"))
 #' 
 #' @export
 
