938dde6a88a4e06afdc29be8baccb5d70c504c6d
sammo3182 <yue-hu-1@uiowa.edu>
2015-06-25 16:38:27 -0500

    update the .rbuildignore and cran-comments

---
+++ cran-comments.md
@@ -0,0 +1,10 @@
+## Test environments
+* local OS X, R 3.2.1
+* local Win 8.1, R 3.2.1
+* Ubuntu 12.04 LTS, R 3.1.3
+
+
+## R CMD check results
+There were no ERRORs or WARNINGs. 
+
+This is the first submission of this package.
---
+++ inst/doc/interplot-vignette.R
@@ -0,0 +1,48 @@
+## ------------------------------------------------------------------------
+data(mtcars)  #load the data
+
+## ------------------------------------------------------------------------
+
+m_cyl <- lm(mpg ~ wt * cyl, data = mtcars)
+summary(m_cyl)
+
+
+
+## ----fig.width = 5-------------------------------------------------------
+library(interplot)
+
+interplot(m = m_cyl, var1 = "cyl", var2 = "wt")
+
+## ----fig.width = 5-------------------------------------------------------
+interplot(m = m_cyl, var1 = "wt", var2 = "cyl")
+
+## ----fig.width = 5-------------------------------------------------------
+interplot(m = m_cyl, var1 = "cyl", var2 = "wt", point = T) +
+  # changing the angle of x labels for a clearer vision
+  theme(axis.text.x  = element_text(angle=90))
+
+## ----fig.width = 5-------------------------------------------------------
+interplot(m = m_cyl, var1 = "cyl", var2 = "wt") + 
+  # Add labels in X and Y axes
+    xlab("Automobile Weight (thousands lbs)") +
+    ylab("Estimated Coefficient for\nNumber of Cylinders") +
+  # Change the background
+    theme_bw() +
+  # Add the title
+    ggtitle("Estimated Coefficient of Engine Cylinders \non Mileage by Automobile Weight") +
+    theme(plot.title = element_text(face="bold")) +
+  # Add a horizontal line at y = 0
+    geom_hline(yintercept = 0, linetype = "dashed")
+
+## ----fig.width = 5.5-----------------------------------------------------
+# Create a fake dataset of conditional effects
+fake <- rnorm(100, 0, 1)
+coef1 <- fake * sample(.5:2.5, 100, replace = T)
+lb <- coef1 - .5
+ub <- coef1 + .5
+
+df_fake <- data.frame(cbind(fake, coef1, lb, ub))
+
+# Use interplot directly with the dataset
+interplot(df_fake)
+
