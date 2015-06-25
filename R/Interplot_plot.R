9cda0bf97072be6aa44921b79e01c7f7ac05ffb0
Frederick Solt <fsolt@mac.com>
2015-06-25 09:05:10 -0500

    Simplify arguments: finish dropping step and seed

--- R/Interplot_plot.R
+++ R/Interplot_plot.R
@@ -1,4 +1,4 @@
-#' Visualize Numeric Statistics of An Interaction
+#' Plot Conditional Coefficients in Models with Interaction Terms
 #' 
 #' Graph based on the data frame of statistics about the conditional effect of an interaciton.
 #' 
@@ -12,7 +12,7 @@
 #' @param steps A numeric value desiring length of the sequence. A non-negative number, which for seq and seq.int will be rounded up if fractional. The default is 100 or the unique categories in the \code{var2} (when it is less than 100. Also see \code{\link{unique}}).
 #' @param point A logical value determining the format of plot. The function produces a point plot when it is \code{TRUE}; otherwise, the fucntion produces a line plot. Both plots have 95\% confidential intervals. The default is \code{FAULSE}.
 #' 
-#' @details \code{interplot.plot} is a S3 method from the \code{interplot}. It can graph the interaction effects based on a statistic data.frame recording the effects in a numeric way.
+#' @details \code{interplot.plot} is a S3 method from the \code{interplot}. It generates plots of conditional coefficients.
 #' 
 #' Because the output function is based on \code{\link[ggplot2]{ggplot}}, any additional arguments and layers supported by \code{ggplot2} can be added with the \code{+}. 
 #' 
@@ -27,7 +27,7 @@ interplot.plot <- function(m, steps = NULL, point = F) {
   if(is.null(steps)) steps <- nrow(m)
   levels <- sort(unique(m$fake))
   
-  if (steps <= 10 | point == T) {
+  if (steps < 10 | point == T) {
     coef.plot <- ggplot(m, aes(x = fake, y = coef1)) + geom_point() + 
       geom_errorbar(aes(ymin = lb, ymax = ub), width = 0) + 
       scale_x_continuous(breaks = levels) + 
