efeb6b60fe9550bfe673adc8f07b57fa97ae3e88
Frederick Solt <fsolt@mac.com>
2016-01-04 11:42:11 -0600

    add more notes

--- dev/predprob_mlm.R
+++ dev/predprob_mlm.R
@@ -41,3 +41,62 @@ t1m1.pp <- ggplot(newdat, aes(x=ginicnty, y=pp, colour=inc)) + geom_line() +
     scale_colour_grey(end=.6) + scale_fill_grey(end=.6) + 
     theme_bw() + theme(legend.position="none")
 #ggsave(file="t1m1_pp.pdf", width=8, height=5.25)
+
+
+### consider a (version of) this code for flat models
+pred_probs <- function(m, var1, var2, var2_vals,
+                       times = 20, sims = 1000) {
+    m_sims <- arm::sim(m, sims)
+    df <- data.frame(m@frame)
+    df[[names(m@flist)]] <- NULL # omit L2 var
+    names(df)[1] <- "(Intercept)" # replace DV with intercept
+    df$`(Intercept)` <- 1
+    
+    # Find name of interaction term
+    ifelse(var1 == var2, var12 <- paste0("I(", var1, "^2)"), 
+           var12 <- paste0(var2, ":", var1))
+    if (!var12 %in% unlist(dimnames(m@pp$X)[2])) 
+        var12 <- paste0(var1, ":", var2)
+    if (!var12 %in% unlist(dimnames(m@pp$X)[2])) 
+        stop(paste("Model does not include the interaction of", var1, "and", 
+                   var2, "."))
+    
+    iv_medians <- summarize_each(df, funs(median(., na.rm = TRUE))) 
+    
+    fake_data <- iv_medians[rep(1:nrow(iv_medians), each=times*length(var2_vals)), ] 
+    fake_data[[var1]] <- with(df, rep(seq(min(get(var1)), max(get(var1)), length.out=times),
+                                      times=length(var2_vals)))
+    fake_data[[var2]] <- rep(var2_vals, each=times)
+    fake_data[[var12]] <- fake_data[[var1]] * fake_data[[var2]]
+    
+    pp <- rowMeans(plogis(data.matrix(fake_data) %*% t(data.matrix(m_sims@fixef))))
+    row_quantiles <- function (x, probs) {
+        naValue <- NA
+        storage.mode(naValue) <- storage.mode(x)
+        nrow <- nrow(x)
+        q <- matrix(naValue, nrow = nrow, ncol = length(probs))
+        if (nrow > 0L) {
+            t <- quantile(x[1L, ], probs = probs)
+            colnames(q) <- names(t)
+            q[1L, ] <- t
+            if (nrow >= 2L) {
+                for (rr in 2:nrow) {
+                    q[rr, ] <- quantile(x[rr, ], probs = probs)
+                }
+            }
+        }
+        else {
+            t <- quantile(0, probs = probs)
+            colnames(q) <- names(t)
+        }
+        q <- drop(q)
+        q
+    }
+    pp_bounds <- row_quantiles(plogis(data.matrix(fake_data) %*% t(data.matrix(m_sims@fixef))), prob = c(.025, .975))
+    pp <- cbind(pp, pp_bounds)
+    pp <- pp*100
+    colnames(pp) <- c("est", "lb", "ub")
+    pp <- cbind(fake_data, pp)
+    pp$income <- as.factor(pp$income)
+    return(pp)
+}
